7f8d70c937f46571e7d7bd73587b395816e46f83
telesyk <telesyk89@gmail.com>
2025-06-13 18:29:41 +0200

    Add new component CurrencySelect

---
+++ src/components/CurrencySelector.jsx
@@ -0,0 +1,30 @@
+import { getCurrenciesMap } from '../utils'
+
+function CurrencyOption({ option }) {
+  return (
+    <option value={option.name}>
+      {option.name} {option.sign}
+    </option>
+  )
+}
+
+export default function CurrencySelector({ currentCurrency, handleSelect }) {
+  if (!currentCurrency || !handleSelect) return
+
+  const currenciesMap = getCurrenciesMap()
+
+  return (
+    <select
+      className="select select-bordered w-full max-w-xs"
+      onChange={event => handleSelect(event.target)}
+      defaultValue={currentCurrency.name}
+    >
+      <option disabled defaultValue={'Select currency'}>
+        Select currency
+      </option>
+      {[...currenciesMap].map(([key, item]) => (
+        <CurrencyOption key={key} option={item} />
+      ))}
+    </select>
+  )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import RatesList from './RatesList'
 import Loader from './Loader'
 import Error from './Error'
 import Header from './Header'
+import CurrencySelect from './CurrencySelect'
 
-export { Footer, RatesList, Loader, Error, Header }
+export { Footer, RatesList, Loader, Error, Header, CurrencySelect }
